706ef133c6ea7a7e4e6b6257bafa811e6769d8bf
Yuh <hataihuy2001@gmail.com>
2022-04-04 14:21:33 +0700

    update

--- resources/views/adminangular/news.blade.php
+++ resources/views/adminangular/news.blade.php
@@ -1,16 +1,6 @@
-<!doctype html>
-<html lang="en" ng-app="myapp" ng-controller="newscontroller">
-  <head>
-    <title>Quan ly tin</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    </head>
-  <body>
+@extends('_layout_admin')
+@section('content')
+<div ng-controller="newscontroller">
     <h1>NEW MANAGEMENT FORM</h1>
     <p><button class="btn btn-primary" ng-click="showmodal(0)"><i class="fa fa-plus"> Create</i></button></p>
     <div>
@@ -70,16 +60,9 @@
                 </div>
             </div>
         </div>
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="/JS/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="/JS/angular.min.js"></script>
-    <script src="/JS/newscontroller.js">
-
-    </script>
-</body>
-</html>
+    </div>
+</div>
+@stop
+@section('js')
+    <script src="/JS/newscontroller.js"></script>
+@stop
